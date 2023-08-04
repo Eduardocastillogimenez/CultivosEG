d7fadec204dbb0baa146cb884fd50e8cf64d5652
Eduardo_cas123 <juegoseduardo09@gmail.com>
2023-08-04 12:37:39 -0400

    fist changes

---
+++ src/view/analisis/index.tsx
@@ -0,0 +1,12 @@
+interface Cultivos {
+    cultivos?: Object;
+}
+function Analisis(props: Cultivos) {
+
+    return (
+        <div>qqqq Analisis</div>
+
+    );
+}
+
+export default Analisis;
---
+++ src/view/principal/index.tsx
@@ -0,0 +1,13 @@
+interface Cultivos {
+    cultivos?: Object;
+}
+
+function Principal(props: Cultivos) {
+
+    return (
+        <div>hhhh Principal</div>
+
+    );
+}
+
+export default Principal;
